6381dfe715923028d96037bb954bcef1bd4b40e8
Ldelara <ligia.delara15@gmail.com>
2023-03-22 19:20:59 -0400

    finish. remove instruction comments

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,28 +1,24 @@
 function configureListeners() {
-  // select img elements
-  let images = document.getElementsByTagName("img");
+  var images = document.getElementsByTagName("img");
 
   for (var i = 0; i < images.length; i++) {
-    // iterate over images and add mouseover event listeners
     document
       .getElementById(images[i].id)
       .addEventListener("mouseover", addOpacity, false);
     document
       .getElementById(images[i].id)
-      .addEventListener("mouseover", removeOpacity, false);
+      .addEventListener("mouseout", removeOpacity, false);
   }
 }
 
 function addOpacity(event) {
-  // add appropriate CSS class
-  if (this.classList.contains("dim")) {
+  if (!this.classList.contains("dim")) {
     this.classList.add("dim");
   }
   getProductInfo(event.target.id);
 }
 
 function removeOpacity(event) {
-  //remove appropriate CSS class
   if (this.classList.contains("dim")) {
     this.classList.remove("dim");
   }
@@ -36,67 +32,56 @@ function removeOpacity(event) {
   event.preventDefault();
 }
 
-function getProductInfo(paintColor) {
+function getProductInfo(partNumber) {
   let price;
   let colorName;
 
-  switch (paintColor) {
+  switch (partNumber) {
     case "pn1":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$14.99";
-      colorName: "Lime Green";
+      price = "$14.99";
+      colorName = "Lime Green";
       updatePrice(colorName, price);
       break;
     case "pn2":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$11.14";
-      colorName: "Medium Brown";
+      price = "$11.14";
+      colorName = "Medium Brown";
       updatePrice(colorName, price);
       break;
     case "pn3":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$22.99";
-      colorName: "Royal Blue";
+      price = "$22.99";
+      colorName = "Royal Blue";
       updatePrice(colorName, price);
       break;
 
     case "pn4":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$13.42";
-      colorName: "Solid Red";
+      price = "$13.42";
+      colorName = "Solid Red";
       updatePrice(colorName, price);
       break;
     case "pn5":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$21.98";
-      colorName: "Solid White";
+      price = "$21.98";
+      colorName = "Solid White";
       updatePrice(colorName, price);
       break;
     case "pn6":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$4.99";
-      colorName: "Solid Black";
+      price = "$15.99";
+      colorName = "Solid Black";
       updatePrice(colorName, price);
       break;
 
     case "pn7":
-      // set variables for price and color name and invoke a function to update the price
-
-      price: "$8.22";
-      colorName: "Solid Cyan";
+      price = "$8.99";
+      colorName = "Solid Cyan";
       updatePrice(colorName, price);
       break;
     case "pn8":
-      // set variables for price and color name and invoke a function to update the price
-
-      price: "$11.99";
-      colorName: "Solid Purple";
+      price = "$16.99";
+      colorName = "Solid Purple";
       updatePrice(colorName, price);
       break;
     case "pn9":
-      // set variables for price and color name and invoke a function to update the price
-      price: "$14.99";
-      colorName: "Solid Yellow";
+      price = "$17.99";
+      colorName = "Solid Yellow";
       updatePrice(colorName, price);
       break;
     default:
